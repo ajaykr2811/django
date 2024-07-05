f19fa0c4597da7f19caa8f8eb3544ac843168b77
ajaykr2811 <38921097+ajaykr2811@users.noreply.github.com>
2024-07-05 16:57:36 +0530

    how to return function, text and html page

--- firstProject/polls/views.py
+++ firstProject/polls/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+def welcome(request):
+    return render(request,"index.html")
+
 def index(request):
     return HttpResponse("Hello world, you are at the polls index")
+
+def error(request):
+    return HttpResponse("This is an error !!")
